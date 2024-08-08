b1b881654c23a14be4eef8ba75505af05b43cd6e
Akshai M Nair <akshai.muraleedharan@subhosting.net>
2024-08-08 14:51:23 +0530

    Added Login Screen

--- aguardion_application/lib/main.dart
+++ aguardion_application/lib/main.dart
@@ -10,9 +10,82 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: LoginScreen()
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF002A41),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/Logosplash.png',
+              width: 238,
+              height: 64,
+            ),
+            const SizedBox(height: 100),
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                color: const Color(0xFF01151E),
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    const SizedBox(height: 20),
+                    const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    const SizedBox(height: 20),
+                    FilledButton(
+                      onPressed: () {
+                       
+                      },
+                      style: FilledButton.styleFrom(
+                        backgroundColor: const Color(0xFF51C0B1),
+                        textStyle: const TextStyle(color: Colors.white),
+                      ),
+                      child: const Text('Sign In'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
